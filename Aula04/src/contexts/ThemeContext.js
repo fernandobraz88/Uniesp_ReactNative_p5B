8571e246530d51ba4b173312a8561da2e50b5125
Fernando Braz <fernandobraz88@gmail.com>
2024-11-27 00:39:58 -0300

    Aula3 e 4

---
+++ Aula04/src/contexts/ThemeContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react'
+
+
+
+const ThemeContext = createContext({
+    theme:'light',
+    toggleTheme:() => {},
+})
+
+export const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState('light')
+    
+    function toggleTheme(){
+        setTheme((currentTheme => (currentTheme ==='light'? 'dark':'light')))
+    }
+    return(
+        <ThemeContext.Provider value={{theme, toggleTheme}}>{children}</ThemeContext.Provider>
+    )
+}
+
+export default ThemeContext
